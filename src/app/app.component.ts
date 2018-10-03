4922d19794f45c53f8992bcf762314a6b7553072
Artstylein <31403222+Artstylein@users.noreply.github.com>
2018-10-03 17:33:23 +0530

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angproject';
+  title = 'Angular Project Biing';
+  itemCount:number=4;
+  btnText:string ="Add an Item";
+  goalText:string ="My First project";
+  goals=[];
+  public AddItemCount(){
+   this.goals.push(this.goalText);
+   this.goalText='';
+   this.itemCount=this.goals.length;
+
+  }
+  constructor(){}
+  ngOnInit(){
+    this.itemCount=this.goals.length;
+  }
+
 }
